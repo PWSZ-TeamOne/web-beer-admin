87bebb53e1f3672920e0d35c33663e6e9a60d73f
Karol Zygadło <karol.zygadlo9713@gmail.com>
2021-01-09 20:56:31 +0100

    config changes

--- src/main.js
+++ src/main.js
@@ -13,7 +13,13 @@ require("firebase/firestore");
 Vue.use(BootstrapVue);
 
 const config = {
-  //place for firebase config
+  apiKey: "AIzaSyANka3bhfzVI2kNARFvcuyTfxaWQRQJGLo",
+  authDomain: "browarapp.firebaseapp.com",
+  databaseURL: "https://browarapp.firebaseio.com",
+  projectId: "browarapp",
+  storageBucket: "browarapp.appspot.com",
+  messagingSenderId: "883017495908",
+  appId: "1:883017495908:web:2c4fa4e6f4558ebae5b671"
 };
 
 
